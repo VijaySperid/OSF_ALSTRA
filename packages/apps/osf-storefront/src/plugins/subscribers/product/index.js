ded26c36b5dd5019886750a5ec71fbe33878ff83
VijaySperid <vijay.hegde@speridian.com>
2022-02-09 14:37:39 +0530

    first commit

---
+++ packages/apps/osf-storefront/src/plugins/subscribers/product/index.js
@@ -0,0 +1,16 @@
+export default ({endpoint, getState, subscribeDispatch}) => {
+    // Get value form the selector
+    // const someValueFromTheState = someSelector(getState());
+   return subscribeDispatch((action, state) => {
+    const {type} = action;
+    
+    // Track getState action
+    if (type === 'getProduct') {
+        // Add connections to other independent endpoints
+        endpoint ('getProduct', {}, state);
+    }
+    // else if (type === 'myOtherAction') {
+    // // Track myOtherAction action
+    // }
+   });
+  };
